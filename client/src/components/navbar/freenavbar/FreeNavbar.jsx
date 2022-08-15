7af590c1e4d3762806d21dab9adcf8398217c1bd
Alexander Shen <70786647+shenalexw@users.noreply.github.com>
2022-08-14 20:24:33 -0700

    fix nav free

--- client/src/components/navbar/freenavbar/FreeNavbar.jsx
+++ client/src/components/navbar/freenavbar/FreeNavbar.jsx
@@ -1,60 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link, useNavigate } from "react-router-dom";
 import '../Navbar.css'
 
 
 export default function FreeNavbar() {
-    const [toggleMenu, setToggleMenu] = useState(false)
-    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
     let navigate = useNavigate();
 
-    const toggleNav = () => {
-        setToggleMenu(!toggleMenu)
-        ham();
-    }
-
-    const ham = () => {
-        const burger = document.querySelector(".burger");
-        burger.classList.toggle("toggle");
-    }
-
-
-    useEffect(() => {
-        const changeWidth = () => {
-            setScreenWidth(window.innerWidth);
-        }
-        window.addEventListener('resize', changeWidth)
-    }, [])
-
     function handleSignIn() {
-        toggleNav();
         navigate("../login", { replace: true });
     }
 
     function handleRegister() {
-        toggleNav();
         navigate("../Register", { replace: true });
     }
-
 
     return (
         <section>
             <nav>
                 <Link to="/" className='navTitle'><p>Password Genny</p></Link>
-                {(toggleMenu || screenWidth > 1000) && (
-                    <ul className="nav-links">
-                        <li onClick={handleSignIn}><button>Sign In</button></li>
-                        <li onClick={handleRegister}><button>Register</button></li>
-                    </ul>
-                )}
-
-                <div className="burger" onClick={toggleNav}>
-                    <div className="line1"></div>
-                    <div className="line2"></div>
-                    <div className="line3"></div>
-                </div>
+                <ul className="nav-links">
+                    <li onClick={handleSignIn}><button>Sign In</button></li>
+                    <li onClick={handleRegister}><button>Register</button></li>
+                </ul>
             </nav>
-
         </section>
     )
 
